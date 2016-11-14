1f88b2753c666757e406e607dcdc670d4098db85
Yu <510292689yy@gmail.com>
2016-11-14 01:28:04 -0500

    Finish weather class

--- app/src/main/java/com/example/yu/team_project_1/MainActivity.java
+++ app/src/main/java/com/example/yu/team_project_1/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.yu.team_project_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button house_setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        house_setting = (Button)findViewById(R.id.house_button);
+        house_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHouse= new Intent(MainActivity.this, HouseSettings.class);
+                startActivity(intentHouse);
+            }
+        });
+
     }
 }
